67c40d87d4493645fbc3a4233819f0494fd9a751
Masafumi Yokoyama <myokoym@gmail.com>
2014-05-19 23:12:38 +0900

    Output warning message when not set an export file

--- lib/grid_pattern_editor/command.rb
+++ lib/grid_pattern_editor/command.rb
@@ -4,7 +4,14 @@ module GridPatternEditor
   class Command
     class << self
       def run(*arguments)
-        window = Window.new(arguments[0])
+        file_name = arguments[0]
+        unless file_name
+          $stderr.puts(<<-END_OF_MESSAGE)
+Warning: not set an export/import file.
+Usage: grid_pattern_editor [FILE_PATH]
+          END_OF_MESSAGE
+        end
+        window = Window.new(file_name)
         window.show
       end
     end
