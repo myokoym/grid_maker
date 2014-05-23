fa3141907cfbb6484645afa4bc0f9561e41d0aac
Masafumi Yokoyama <myokoym@gmail.com>
2014-05-24 00:03:38 +0900

    Fix a typo
    controll_panel ->
    control_panel

--- lib/grid_pattern_editor.rb
+++ lib/grid_pattern_editor.rb
@@ -2,7 +2,7 @@ require "grid_pattern_editor/version"
 require "grid_pattern_editor/window"
 require "grid_pattern_editor/board"
 require "grid_pattern_editor/cell"
-require "grid_pattern_editor/controll_panel"
+require "grid_pattern_editor/control_panel"
 require "grid_pattern_editor/button"
 require "grid_pattern_editor/base"
 
